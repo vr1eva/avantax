55e2b6cf85c0fb2f6250377d20424bcc07636395
vr1 <113930888+vr1eva@users.noreply.github.com>
2023-01-13 01:48:36 +0000

    added company benefits section

--- pages/components/Benefits.jsx
+++ pages/components/Benefits.jsx
@@ -5,10 +5,23 @@ import buildings from "../assets/buildings.svg"
 import convert from "../assets/convert.svg"
 import cupheart from "../assets/cupheart.svg"
 import status from "../assets/status.svg"
+import medal from "../assets/medal.svg"
+import ranking from "../assets/ranking.svg"
+import award from "../assets/award.svg"
+import money from "../assets/money.svg"
 import Image from "next/image"
+const icons = { buildings, convert, cupheart, status, medal, ranking, award, money }
 
-const icons = { buildings, convert, cupheart, status }
-const buttonName = "Comienza a invertir"
+
+export default function() {
+  return (
+    <section className={styles.benefits}>
+      <Benefits />
+      <CompanyBenefits />
+      <CallToAction />
+    </section>
+  )
+}
 
 export const BENEFITS = [
   {
@@ -33,10 +46,13 @@ export const BENEFITS = [
   },
 ]
 
-export default function Benefits() {
+
+
+function Benefits() {
+  const heading = "¿Qué beneficios brindan las Obras por Impuestos al país?"
   return (
-    <section className={styles.benefits}>
-      <h2 className={styles.heading}>¿Qué beneficios brindan las Obras por Impuestos al país?</h2>
+    <div className={styles.benefit}>
+      <h2 className={styles.heading}>{heading}</h2>
       <div className={styles.cards}>
         {BENEFITS.map(item => (
           <Card
@@ -54,9 +70,63 @@ export default function Benefits() {
         ))}
       </div>
 
-      <div className={styles.buttonRow}>
-        <Button variant="primary" onClick={() => { }} name={buttonName} />
+    </div>
+  )
+}
+
+const COMPANY_BENEFITS = [
+  {
+    title: "Responsabilidad Social",
+    description: "Pueden manejar sus programas de Responsabilidad Social a coste cero.",
+    icon: "medal"
+  },
+  {
+    title: "Mejor posicionamiento",
+    description: "Pueden mejorar su reputación y posicionamiento también a coste cero.",
+    icon: "ranking"
+  },
+  {
+    title: "Competitividad",
+    description: "Pueden destinar sus impuestos para obras puntuales, las cuales podrían elevar la competitividad de la comunidad y de la empresa.",
+    icon: "award"
+  },
+  {
+    title: "Retorno de utilidades",
+    description: "Pueden obtener un retorno de utilidades si se encargan de la ejecución de obra.",
+    icon: "money"
+  },
+]
+
+function CompanyBenefits() {
+  const heading = "¿Qué beneficios brindan las Obras por Impuestos para las empresas que las financian?"
+  return (
+    <div className={styles.benefit}>
+      <h2 className={styles.heading}>{heading}</h2>
+      <div className={styles.cards}>
+        {COMPANY_BENEFITS.map(item => (
+          <Card
+            key={item.title}
+            title={item.title}
+            description={item.description}
+          >
+            <Image
+              alt={"Benefit" + item.title}
+              src={icons[item.icon]}
+              width={70}
+              height={70}
+            />
+          </Card>
+        ))}
       </div>
-    </section>
+    </div>
+  )
+}
+
+function CallToAction() {
+  const buttonName = "Comienza a invertir"
+  return (
+    <div className={styles.buttonRow}>
+      <Button variant="primary" onClick={() => { }} name={buttonName} />
+    </div>
   )
 }
