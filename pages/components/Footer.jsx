e53d98eaaffe582f358631002c0a888f50d969ff
vr1 <113930888+vr1eva@users.noreply.github.com>
2023-01-17 09:10:55 +0000

    fixed empty spacing

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -8,49 +8,71 @@ import { Inter } from '@next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const details = [{ label: "Número de contacto", value: "+51 944 060 980" }, { label: "Correo electrónico", value: "rtorres@avantax.pe" }, { label: "Dirección", value: "Pasaje Mártir Olaya 129,  Oficinas 1905 - Miraflores, Lima - Perú" }]
+
+const links = [
+  { title: "Preguntas frecuentes", href: "#" },
+  { title: "Sobre Nosotros", href: "#" },
+  { title: "Saca una cita con nosotros", href: "#" },
+]
 
 function Footer() {
-
-  const details = [{ label: "Número de contacto", value: "+51 944 060 980" }, { label: "Correo electrónico", value: "rtorres@avantax.pe" }, { label: "Dirección", value: "Pasaje Mártir Olaya 129,  Oficinas 1905 - Miraflores, Lima - Perú" }]
-
-  const links = [
-    { title: "Preguntas frecuentes", href: "#" },
-    { title: "Sobre Nosotros", href: "#" },
-    { title: "Saca una cita con nosotros", href: "#" },
-  ]
   return (
     <div className={styles.footer} >
       <div className={styles.row}>
-        <div className={styles.social}>
-          <Image src={logo} />
-          <div className={styles.socialDivider} />
-          <div className={styles.socialMediaLinks}>
-            <a href="#"><Image width={24} height={24} src={linkedin} /></a>
-            <a href="#"><Image width={24} height={19.5} src={twitter} /></a>
-            <a href="#"><Image width={24} height={24} src={instagram} /></a>
-          </div>
-        </div>
 
-        <div className={styles.details}>
-          {details.map(det => (
-            <div>
-              <p className={styles.detailLabel}>{det.label}</p>
-              <p className={styles.detailValue}>{det.value}</p>
-            </div>
-          ))}
-        </div>
-
-        <div className={styles.links}>
-          {links.map(link => (
-            <a className={styles.link} href={link.href}>{link.title}</a>
-          ))}
-        </div>
-
+        <Social />
+        <Details />
+        <Links />
       </div>
       <div className={styles.divider} />
-      <p className={`${inter.className} ${styles.disclaimer}`}>Avantax © Todos los derechos reservados 2023</p>
+      <Copyright />
     </div>
   );
 };
+
+function Social() {
+  return (
+    <div className={styles.social}>
+      <Image src={logo} />
+      <div className={styles.socialDivider} />
+      <div className={styles.socialMediaLinks}>
+        <a href="#"><Image width={24} height={24} src={linkedin} /></a>
+        <a href="#"><Image width={24} height={19.5} src={twitter} /></a>
+        <a href="#"><Image width={24} height={24} src={instagram} /></a>
+      </div>
+    </div>
+  )
+}
+
+function Details() {
+  return (
+    <div className={styles.details}>
+      {details.map(det => (
+        <div>
+          <p className={styles.detailLabel}>{det.label}</p>
+          <p className={styles.detailValue}>{det.value}</p>
+        </div>
+      ))}
+    </div>
+
+  )
+}
+
+function Links() {
+  return (
+    <div className={styles.links}>
+      {links.map(link => (
+        <a className={styles.link} href={link.href}>{link.title}</a>
+      ))}
+    </div>
+  )
+}
+
+function Copyright() {
+  return (
+    <p className={`${inter.className} ${styles.disclaimer}`}>Avantax © Todos los derechos reservados 2023</p>)
+}
+
 
 export default Footer;
