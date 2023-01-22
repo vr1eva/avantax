f26db8182926478d5d7440a2c03309997b46ce4b
vr1 <113930888+vr1eva@users.noreply.github.com>
2023-01-22 14:12:07 +0000

    adapt hero image sizing

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -1,18 +1,20 @@
 import styles from "./Hero.module.scss"
 import Image from 'next/image'
 import Button from "./Button"
-import hero from "../assets/hero.jpg"
+import heroImage from "../assets/hero.jpg"
 
 export default function Hero() {
   return (
-    <div className={styles.hero}>
+    <div className={styles.hero} style={{
+      backgroundImage: `linear-gradient(360deg, rgba(0, 0, 0, 0.59) 0%, rgba(0, 0, 0, 0) 67.72%, rgba(0, 0, 0, 0) 97.16%), url(${heroImage.src})`,
+    }}>
       <div className={styles.description}>
         <h2 className={styles.heading}><b>Le damos propósito</b> a tus impuestos</h2>
         <p className={styles.subheading}>Somos una consultora especializada en la implementación de proyectos de inversión pública mediante el mecanismo de Obras por Impuestos</p>
-        <Button variant="primary" name="¿Qué son las Obras por Impuestos?" onClick={() => { }}></Button>
+        <div className={styles.cta}>
+          <Button variant="primary" name="¿Qué son las Obras por Impuestos?" onClick={() => { }}></Button>
+        </div>
       </div>
-      <Image className={styles.image} width={1440} height={678} alt="Niño tomando agua" src={hero} />
-
     </div>
   );
 };
